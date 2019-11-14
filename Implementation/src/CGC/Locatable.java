d28ed74e20372b2f4846b4924ab2956db2250270
Siri Khalsa <skhalsa10@live.com>
2019-11-14 13:50:35 -0700

    Added documentation  for the Locatable interface

--- Implementation/src/CGC/Locatable.java
+++ Implementation/src/CGC/Locatable.java
@@ -2,6 +2,16 @@ package CGC;
 
 import java.awt.*;
 
+/**
+ * The purpose of this interface is to enforce location services.
+ * This forces the behavior of having simulated GPS. This interfaces enforces the ping behavior of the gps
+ */
 public interface Locatable {
+
+    /**
+     * This will give the behavior to check for the current location at any time. It is up to the
+     * implementing object to keep track of the point and also how to update the point location.
+     * @return a Point representing the current x and y location of the implementing object.
+     */
     Point getLocation();
 }
