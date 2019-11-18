61439e1949771815b898b10862d9723052de03f1
Siri Khalsa <skhalsa10@live.com>
2019-11-17 17:29:27 -0700

    added skeleton for CGCStation realted

--- Implementation/src/cgc/cgcstation/CGCGUI.java
+++ Implementation/src/cgc/cgcstation/CGCGUI.java
@@ -1,4 +1,46 @@
 package cgc.cgcstation;
 
-public class CGCGUI {
+import cgc.Communicator;
+import cgc.messages.Message;
+import javafx.animation.AnimationTimer;
+import javafx.stage.Stage;
+
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
+    long lastUpdate = 0;
+    PriorityBlockingQueue<Message> messages;
+
+    public CGCGUI(Stage primaryStage, CGCStation cgcStation) {
+        Thread messageThread = new Thread(this);
+        messageThread.start();
+    }
+
+    @Override
+    public void handle(long now) {
+        //there are 1000 miliseconds in a second. if we divide this by 60 there
+        // are 16.666667 ms between frame draws
+        if (now - lastUpdate >= 16_667_000) {
+
+
+
+            // helped to stabalize the rendor time
+            lastUpdate = now;
+        }
+    }
+
+    private void processMessage(Message m){
+        //TODO
+    }
+
+
+    @Override
+    public void sendMessage(Message m) {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
 }
