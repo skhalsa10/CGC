7053c1975a90ca40f3bb92ff37d08075320813e7
Siri Khalsa <skhalsa10@live.com>
2019-11-21 22:47:21 -0700

    working on gui

--- Implementation/src/cgc/cgcstation/CGCGUI.java
+++ Implementation/src/cgc/cgcstation/CGCGUI.java
@@ -8,26 +8,29 @@ import javafx.stage.Stage;
 import java.util.concurrent.PriorityBlockingQueue;
 
 public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
-    long lastUpdate = 0;
-    PriorityBlockingQueue<Message> messages;
+
+    private long lastUpdate = 0;
+    private PriorityBlockingQueue<Message> messages;
+    private Thread messageThread;
+    private boolean isRunning;
+
+    //GUI stuff
+    private Stage stage;
+
 
     public CGCGUI(Stage primaryStage, CGCStation cgcStation) {
-        Thread messageThread = new Thread(this);
+
+        isRunning = true;
+        messageThread = new Thread(this);
         messageThread.start();
+
+        //GUI
+        this.stage = primaryStage;
+        stage.setTitle("Cretaceous Gardens Controller");
+
+        this.start();
     }
 
-    @Override
-    public void handle(long now) {
-        //there are 1000 miliseconds in a second. if we divide this by 60 there
-        // are 16.666667 ms between frame draws
-        if (now - lastUpdate >= 16_667_000) {
-
-
-
-            // helped to stabalize the rendor time
-            lastUpdate = now;
-        }
-    }
 
     private void processMessage(Message m){
         //TODO
@@ -36,11 +39,33 @@ public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
 
     @Override
     public void sendMessage(Message m) {
-
+        messages.put(m);
     }
 
     @Override
     public void run() {
+        while(isRunning){
+            try {
+                Message m = messages.take();
+                processMessage(m);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    /**
+     * this is used to paint the gui on the screen. it is needed to draw the animation.
+     * @param now
+     */
+    @Override
+    public void handle(long now) {
+        //there are 1000 miliseconds in a second. if we divide this by 60 there
+        // are 16.666667 ms between frame draws
+        if (now - lastUpdate >= 16_667_000) {
+
+            // helped to stabalize the rendor time
+            lastUpdate = now;
+        }
     }
 }
