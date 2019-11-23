495e777a564c2b130d456f004458a6c12fb64835
JamesMc <jamesmc@correo.ugr.es>
2019-11-23 13:15:45 -0700

    TransactionLog.v1

--- Implementation/src/cgc/kioskmanager/TransactionLogger.java
+++ Implementation/src/cgc/kioskmanager/TransactionLogger.java
@@ -1,4 +1,6 @@
 package cgc.kioskmanager;
+import java.util.ArrayList;
+import java.time.LocalTime;
 
 /**
  * The purpose of this class is to help analyze the finances generated
@@ -9,7 +11,43 @@ package cgc.kioskmanager;
  */
 public class TransactionLogger {
 
+    //Variables related with finances.
+    private Double totalBenefits;
+    private ArrayList<Double> mensualBenefits;
+    private Date date;
+    
     public TransactionLogger() {
+        initializeMensualBenefits();
+        totalBenefits = 0.0;
+    }
 
+    //Register the sale of a ticket.
+    public void registerSale(double amount, Date purcharseDate){
+        totalBenefits+= amount;
+
+        //Mensual approach
+    }
+
+
+    //Initialize the Mensual Benefits Array to 0.
+    private void initializeMensualBenefits(){
+        mensualBenefits = new ArrayList<>(12);
+
+        //Set the benefits of each month to 0.
+        for(int i = 0; i < 12; i++){
+            mensualBenefits.set(i, 0.0);
+        }
+    }
+
+    //Return the beneficts from that month.
+    public Double getMensualBenefits(int month){
+
+        return(mensualBenefits.get(month));
+    }
+
+    //Return the totalBeneficts of the park.
+    public Double getTotalBenefits(){
+
+        return(totalBenefits);
     }
 }
