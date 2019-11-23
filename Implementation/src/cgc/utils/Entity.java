d65a79a69ac62fcf21b7c91b098b7868fc94d36a
Siri Khalsa <skhalsa10@live.com>
2019-11-22 23:02:38 -0700

    updated Entity enum

--- Implementation/src/cgc/utils/Entity.java
+++ Implementation/src/cgc/utils/Entity.java
@@ -4,5 +4,5 @@ package cgc.utils;
  * Feel free to add more appropriate enums as needed.
  */
 public enum Entity {
-    TOKEN, CAR, KIOSK
+    EMPLOYEE_TOKEN,GUEST_TOKEN, TOUR_VEHICLE, PATROL_VEHICLE,  KIOSK, TREX
 }
