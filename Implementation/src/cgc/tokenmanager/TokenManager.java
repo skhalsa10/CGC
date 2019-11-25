154cf17127f9b484b088c383a6e7e61c501ea127
Matthew Earl Stone <mestone@unm.edu>
2019-11-24 22:58:49 -0700

    Replace TokenManager.java with what I have so far I need to change where I am working from so moving these here for the time being.

--- Implementation/src/cgc/tokenmanager/TokenManager.java
+++ Implementation/src/cgc/tokenmanager/TokenManager.java
@@ -2,8 +2,9 @@ package cgc.tokenmanager;
 
 import cgc.CGC;
 import cgc.utils.Communicator;
-import cgc.utils.messages.Message;
+import cgc.utils.messages.*;
 
+import java.util.LinkedList;
 import java.util.concurrent.PriorityBlockingQueue;
 
 /**
@@ -43,10 +44,19 @@ import java.util.concurrent.PriorityBlockingQueue;
  *      1. It will respond with a message with the current health of all tokens
  *
  */
-public class TokenManager extends Thread implements Communicator {
+public class TokenManager extends Thread implements Communicator
+{
 
     private CGC cgc;
     private PriorityBlockingQueue<Message> messages;
+
+    //variables I added
+    private boolean isRunning = true;
+    private boolean emergency = false;
+    private LinkedList<GuestToken> guestTokens = new LinkedList<>();
+    private LinkedList<EmployeeToken> employeeTokens = new LinkedList<>();
+    private int tokenID = 0;
+    private int employee_count = 5;
 
     public  TokenManager(CGC cgc){
         messages = new PriorityBlockingQueue<>();
@@ -56,17 +66,83 @@ public class TokenManager extends Thread implements Communicator {
 
 
     @Override
-    public void run() {
+    public void run()
+    {
         //TODO this will loop and wait on the messages queue and
         // call processMessage(m) when a message arrives
+        //create employee tokens
+        for (int i = 0; i < 5; i++)
+        {
+            EmployeeToken tmp = new EmployeeToken(tokenID, this,);
+            tokenID++;
+        }
+        while (isRunning)
+        {
+            try
+            {
+                Message m = this.messages.take();
+                processMessage(m);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("TokenManager shutting down.");
     }
 
     @Override
-    public synchronized void sendMessage(Message m) {
+    public synchronized void sendMessage(Message m)
+    {
         //TODO place Message inside of messages Queue
+        messages.put(m);
     }
 
-    private void processMessage(Message m){
+    private void processMessage(Message m)
+    {
         //TODO  respond accordingly to m using instanceof
+        if (m instanceof ShutDown)
+        {
+            //shut down all tokens then allow for shutdown of self.
+            //loop through tokens shutting them all down
+            isRunning = false;
+        }
+        else if(m instanceof EnterEmergencyMode)
+        {
+            emergency = true;
+            //loop through tokens and tell them emergency mode
+        }
+        else if (m instanceof ExitEmergencyMode)
+        {
+            emergency = false;
+            //loop through tokens and free them from emergency mode
+        }
+        else if (m instanceof RequestToken)
+        {
+            if (emergency)
+            {
+                System.out.println("Attempted to create token but didn't because the system is in emergency mode");
+            }
+            else
+            {
+                GuestToken tmp = new GuestToken(tokenID, this, );
+                guestTokens.add(tmp);
+                TokenInfo passInfo = new TokenInfo(tmp.tokenID, tmp.GPSLocation, true);
+                sendMessage(passInfo);
+                tokenID++;
+            }
+        }
+        else if (m instanceof UpdatedLocation)
+        {
+            //
+        }
+        else if (m instanceof CGCRequestHealth)
+        {
+            //
+        }
+        else if (m instanceof UpdatedHealth)
+        {
+            //
+        }
     }
 }
