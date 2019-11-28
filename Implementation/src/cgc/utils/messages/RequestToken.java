0c97818d41efd56772e5b41e6b72ba8d99b65da1
JamesMc <jamesmc@correo.ugr.es>
2019-11-27 23:23:42 -0700

    Modify the RequestToken message

--- Implementation/src/cgc/utils/messages/RequestToken.java
+++ Implementation/src/cgc/utils/messages/RequestToken.java
@@ -5,12 +5,17 @@ This message is sent by the Kiosk Manager to the CGC because a token has been pu
 a pay kiosk.
 */
 
+import javafx.geometry.Point2D;
+
 public class RequestToken implements Message {
     // TODO: add appropriate properties associated with requesting token.
+    private Point2D location;
 
-    public RequestToken() {
-
+    public RequestToken(Point2D location) {
+        this.location = location;
     }
 
-
+    public Point2D getLocation() {
+        return location;
+    }
 }
