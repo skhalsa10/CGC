485197959f43aed031475d07c9d9bd7ce0a71f2a
Siri Khalsa <skhalsa10@live.com>
2019-11-29 00:54:03 -0700

    working on finance screen have everythign but the log of each sale

--- Implementation/src/cgc/cgcstation/CGCGUI.java
+++ Implementation/src/cgc/cgcstation/CGCGUI.java
@@ -1,5 +1,6 @@
 package cgc.cgcstation;
 
+import cgc.kioskmanager.TicketPrice;
 import cgc.utils.Communicator;
 import cgc.utils.MapInfo;
 import cgc.utils.messages.*;
@@ -12,14 +13,18 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.*;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
 
 
+import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
 
 public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
@@ -43,6 +48,19 @@ public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
     private Scene mainScene;
     private HBox mainRoot;
 
+    //finance screen
+    private Scene financeScene;
+    private VBox financeRoot;
+    private HBox finBtnBox;
+    private HBox titleBox;
+    private Text financeTitle;
+    private ScrollPane financeScrollPane;
+    private HBox totalBox;
+    private Text childTokensSold;
+    private Text adultTokensSold;
+    private Text seniorTokensSold;
+
+
     //animated map
     private Canvas canvas;
     private GraphicsContext gc;
@@ -56,23 +74,24 @@ public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
     private Button enterEmergency;
     private Button viewHealth;
     private Button viewFinances;
+    private Button viewMain;
+    private Button financeRefresh;
 
     //State stuff
     private Point2D  TRexLoc;
     private boolean  TRexHealth = true;
-    private HashMap<Integer,Point2D> tourLocations;
-    private HashMap<Integer,Point2D> patrolLocations;
-    private HashMap<Integer,Point2D> employeeLocations;
-    private HashMap<Integer,Point2D> guestLocations;
-    private HashMap<Integer,Point2D> kioskLocations;
+    private ConcurrentHashMap<Integer,Point2D> tourLocations;
+    private ConcurrentHashMap<Integer,Point2D> patrolLocations;
+    private ConcurrentHashMap<Integer,Point2D> employeeLocations;
+    private ConcurrentHashMap<Integer,Point2D> guestLocations;
+    private ConcurrentHashMap<Integer,Point2D> kioskLocations;
 
-    private HashMap<Integer,Boolean> tourHealth;
-    private HashMap<Integer,Boolean> patrolHealth;
-    private HashMap<Integer,Boolean> employeeHealth;
-    private HashMap<Integer,Boolean> guestHealth;
-    private HashMap<Integer,Boolean> kioskHealth;
-    private boolean  elctricFenceHealth = true;
-
+    private ConcurrentHashMap<Integer,Boolean> tourHealth;
+    private ConcurrentHashMap<Integer,Boolean> patrolHealth;
+    private ConcurrentHashMap<Integer,Boolean> employeeHealth;
+    private ConcurrentHashMap<Integer,Boolean> guestHealth;
+    private ConcurrentHashMap<Integer,Boolean> kioskHealth;
+    private LinkedBlockingQueue<UpdatedFinanceInfo> financeState;
 
 
 
@@ -83,23 +102,25 @@ public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
         isInEmergency = false;
         messageThread = new Thread(this);
         messages = new PriorityBlockingQueue<>();
+        financeState = new LinkedBlockingQueue<>();
         currentScreen = Screen.MAIN;
 
-        tourLocations = new HashMap<>();
-        patrolLocations = new HashMap<>();
-        employeeLocations = new HashMap<>();
-        guestLocations = new HashMap<>();
-        kioskLocations = new HashMap<>();
+        tourLocations = new ConcurrentHashMap<>();
+        patrolLocations = new ConcurrentHashMap<>();
+        employeeLocations = new ConcurrentHashMap<>();
+        guestLocations = new ConcurrentHashMap<>();
+        kioskLocations = new ConcurrentHashMap<>();
 
-        tourHealth = new HashMap<>();
-        patrolHealth = new HashMap<>();
-        employeeHealth = new HashMap<>();
-        guestHealth = new HashMap<>();
-        kioskHealth = new HashMap<>();
+        tourHealth = new ConcurrentHashMap<>();
+        patrolHealth = new ConcurrentHashMap<>();
+        employeeHealth = new ConcurrentHashMap<>();
+        guestHealth = new ConcurrentHashMap<>();
+        kioskHealth = new ConcurrentHashMap<>();
 
         //GUI
         this.stage = primaryStage;
         stage.setTitle("Cretaceous Gardens Controller");
+
 
         //init main stuff
         mainRoot = new HBox();
@@ -107,6 +128,8 @@ public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
         canvasContainer = new StackPane();
         canvas = new Canvas(MapInfo.MAP_WIDTH,MapInfo.MAP_HEIGHT);
         gc = canvas.getGraphicsContext2D();
+
+
 
         //button stuff
         leftBPane = new VBox();
@@ -150,10 +173,23 @@ public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
         });
         viewFinances = new Button("View\nFinances");
         viewFinances.getStyleClass().add("viewFinances-button");
+        viewFinances.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                cgcStation.sendMessage(new RequestFinanceInfo());
+                currentScreen = Screen.FINANCES;
+            }
+        });
+
+
+
+
+
         viewHealth.setMinWidth(200);
         viewFinances.setMinWidth(200);
         enterEmergency.setMinWidth(200);
         exitEmergency.setMinWidth(200);
+
 
 
         //populate stuff
@@ -163,6 +199,51 @@ public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
         canvasContainer.getStyleClass().add("canvasContainer");
         mainRoot.getChildren().addAll(leftBPane,canvasContainer,rightBPane);
 
+        //initializethe Finance stuff
+        financeRoot = new VBox();
+        titleBox = new HBox();
+        financeScrollPane = new ScrollPane();
+        financeScrollPane.getStyleClass().add("financeScrollPane");
+
+        //totals box set up
+        totalBox = new HBox();
+
+
+        viewMain = new Button("<");
+        viewMain.getStyleClass().add("viewMain-button");
+        viewMain.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                cgcStation.sendMessage(new CGCRequestLocation());
+                currentScreen = Screen.MAIN;
+            }
+        });
+        financeRefresh = new Button("\u21BB");
+        financeRefresh.getStyleClass().add("viewMain-button");
+        financeRefresh.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                cgcStation.sendMessage(new RequestFinanceInfo());
+
+            }
+        });
+        finBtnBox = new HBox();
+        Pane s3 = new Pane();
+        finBtnBox.getChildren().addAll(viewMain,s3,financeRefresh);
+        finBtnBox.setHgrow(s3, Priority.ALWAYS);
+
+        financeTitle = new Text("Finances");
+        financeTitle.getStyleClass().add("financeTitle");
+        Pane s1 = new Pane();
+        Pane s2 = new Pane();
+        titleBox.getChildren().addAll(s1, financeTitle,s2);
+        titleBox.setAlignment(Pos.BOTTOM_CENTER);
+        titleBox.setHgrow(s1, Priority.ALWAYS);
+        titleBox.setHgrow(s2, Priority.ALWAYS);
+        financeRoot.getChildren().addAll(finBtnBox,titleBox, financeScrollPane,totalBox);
+        financeRoot.setVgrow(financeScrollPane,Priority.ALWAYS);
+
+
         //TESTING TODO REMOVE GOR GOLIVE
         TRexLoc = new Point2D(MapInfo.CENTER_TREX_PIT.getX(), MapInfo.CENTER_TREX_PIT.getY());
 
@@ -170,7 +251,12 @@ public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
 
         //create scene and set style sheet
         mainScene = new Scene(mainRoot, mainRoot.getMaxWidth(), MapInfo.MAP_HEIGHT-50);
+        financeScene = new Scene(financeRoot, mainRoot.getMaxWidth(), MapInfo.MAP_HEIGHT-50);
         mainScene.getStylesheets().add("cgc/cgcstation/GUI.css");
+        financeScene.getStylesheets().add("cgc/cgcstation/GUI.css");
+
+        stage.setMinWidth(MapInfo.MAP_WIDTH+400);
+        stage.setMinHeight(MapInfo.MAP_HEIGHT-50);
 
         //display the stage
         stage.setScene(mainScene);
@@ -276,8 +362,14 @@ public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
             isInEmergency = true;
         }
         else if(m instanceof UpdatedFinanceInfo){
+            System.out.println("Received Updated Finance info in GUI");
             UpdatedFinanceInfo m2 = (UpdatedFinanceInfo)m;
-            //TODO switch to finacne screen and display finance info
+            try {
+                financeState.put(m2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
         }
         else if(m instanceof ShutDown){
             System.out.println("GUI is Shutting down");
@@ -287,6 +379,27 @@ public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
         else{
             System.out.println("Cant process this message sorry");
         }
+    }
+
+    private void buildTotalBox(ArrayList<Integer> typeTicketsSold, Double total) {
+        int childTicks = typeTicketsSold.get(TicketPrice.CHILDREN.ordinal());
+        int adultTicks = typeTicketsSold.get(TicketPrice.ADULT.ordinal());
+        int seniorTicks = typeTicketsSold.get(TicketPrice.SENIOR.ordinal());
+        Text c = new Text("Total Tokens: Child - "+ childTicks+" , ");
+        Text a = new Text("Adult - "+adultTicks+" , ");
+        Text s = new Text("Senior - "+seniorTicks);
+        DecimalFormat format = new DecimalFormat("#,###.00");
+        Text t = new Text("Total Sales: $" + format.format(total));
+        c.getStyleClass().add("Finance-Text");
+        a.getStyleClass().add("Finance-Text");
+        s.getStyleClass().add("Finance-Text");
+        t.getStyleClass().add("Finance-Text");
+        Pane spacer = new Pane();
+        //clear the box
+        totalBox.getChildren().clear();
+        //read with updated info
+        totalBox.getChildren().addAll(c,a,s, spacer,t);
+        totalBox.setHgrow(spacer,Priority.ALWAYS);
     }
 
 
@@ -301,6 +414,7 @@ public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
         if (now - lastUpdate >= 16_667_000) {
 
             if(currentScreen == Screen.FINANCES){
+                renderFinanceScreen();
 
             }else if(currentScreen == Screen.HEALTH) {
 
@@ -314,7 +428,21 @@ public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
         }
     }
 
+    private void renderFinanceScreen() {
+        stage.setScene(financeScene);
+        UpdatedFinanceInfo m;
+        if((m= financeState.poll())!=null){
+             buildTotalBox(m.getTypeTicketsSold(),m.getTotalBenefits());
+             buildItemizedList(m.getMonthsBenefits());
+        }
+    }
+
+    private void buildItemizedList(ArrayList<Double> monthsBenefits) {
+
+    }
+
     private synchronized void renderMainScreen() {
+        stage.setScene(mainScene);
         //first thing we need to do is paint the background of the map
         gc.setFill(MapInfo.CANVASBACKGROUND);
         gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
@@ -420,7 +548,13 @@ public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
 
     }
 
-    private void renderNodeAndHealth(Integer i, HashMap<Integer, Point2D> locations, HashMap<Integer, Boolean> health) {
+    /**
+     * Helper function for renderMainScreen
+     * @param i
+     * @param locations
+     * @param health
+     */
+    private void renderNodeAndHealth(Integer i, ConcurrentHashMap<Integer, Point2D> locations, ConcurrentHashMap<Integer, Boolean> health) {
         Point2D p = locations.get(i);
         gc.fillOval(p.getX(),p.getY(),6,6);
         if(health.get(i)!= null) {
@@ -430,6 +564,12 @@ public class CGCGUI extends AnimationTimer implements Runnable, Communicator {
         }
     }
 
+    /**
+     * helper function for renderMainScreen
+     * @param healthStatus
+     * @param x
+     * @param y
+     */
     private void renderHealth(Boolean healthStatus, double x, double y) {
         if(healthOverlayIsOn){
 
