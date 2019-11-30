dfba793f744cefd0c31cbe3ab8017acea5e2c032
Matthew Earl Stone <mestone@unm.edu>
2019-11-30 08:16:30 -0700

    Replace EmployeeToken.java

--- Implementation/src/cgc/tokenmanager/EmployeeToken.java
+++ Implementation/src/cgc/tokenmanager/EmployeeToken.java
@@ -1,9 +1,14 @@
 package cgc.tokenmanager;
 
+import cgc.utils.Entity;
+import cgc.utils.MapInfo;
 import cgc.utils.messages.*;
 import javafx.geometry.Point2D;
 
 import java.awt.*;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * this token encapsulates the behavior os the Employee. in the real world it would just report data to the token
@@ -38,10 +43,19 @@ public class EmployeeToken extends Token
     private boolean isRunning = true;
     private boolean emergency = false;
 
+    //my variables
+    private Timer timer;
+
+    //Borrowed stuff
+    private enum Direction {
+        EAST, WEST, NORTH, SOUTH, NORTHEAST, NORTHWEST, SOUTHEAST, SOUTHWEST
+    }
+
     @Override
     public void sendMessage(Message m)
     {
         //TODO place this message in messages queue
+        tokenManager.sendMessage(m);
     }
 
 
@@ -70,7 +84,71 @@ public class EmployeeToken extends Token
     protected void startTokenTimer()
     {
         //TODO start token timer here and use a timer task with it.
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                MoveTRex move = new MoveTRex();
+                messages.put(move);
+            }
+        };
+        // schedules after every second.
+        this.timer.schedule(task, 0, 1000);
     }
+
+    //=========================================================================
+    //borrowed
+    private Direction randomDirection() {
+        Direction[] dir = Direction.values();
+        Random rand = new Random();
+        // generate random number between 0-7.
+        return dir[rand.nextInt(dir.length)];
+    }
+
+    private Point2D changeCoordinates(double oldX, double oldY) {
+        Direction directionToMove = randomDirection();
+        Point2D changedPoint = null;
+
+        switch (directionToMove) {
+            case EAST:
+                changedPoint = new Point2D(oldX + 3.0, oldY);
+                break;
+            case WEST:
+                changedPoint = new Point2D(oldX - 3.0, oldY);
+                break;
+            case NORTH:
+                changedPoint = new Point2D(oldX, oldY + 3.0);
+                break;
+            case SOUTH:
+                changedPoint = new Point2D(oldX, oldY - 3.0);
+                break;
+            case NORTHEAST:
+                changedPoint = new Point2D(oldX + 3.0, oldY + 3.0);
+                break;
+            case NORTHWEST:
+                changedPoint = new Point2D(oldX - 3.0, oldY + 3.0);
+                break;
+            case SOUTHEAST:
+                changedPoint = new Point2D(oldX + 3.0, oldY - 3.0);
+                break;
+            case SOUTHWEST:
+                changedPoint = new Point2D(oldX - 3.0, oldY - 3.0);
+                break;
+        }
+        return changedPoint;
+    }
+
+    private boolean isLegalMove(double x, double y) {
+        // creating bounds.
+        double leftX = MapInfo.UPPER_LEFT_TREX_PIT.getX();
+        double rightX = MapInfo.UPPER_RIGHT_TREX_PIT.getX();
+
+        if (x < leftX || x > rightX || y < 0 || y > MapInfo.TREX_PIT_HEIGHT) {
+            return false;
+        }
+
+        return true;
+    }
+    //=============================================================
 
     @Override
     protected synchronized void processMessage(Message m)
@@ -90,9 +168,18 @@ public class EmployeeToken extends Token
         }
         else if (m instanceof CGCRequestHealth)
         {
-            //for now I'm putting true in because I don't forsee needing a health status during the simulation, at least for v1
-            //also may need to outright commentout this line to make this all work for v1
             //TODO-sendMessage(new UpdatedHealth(this.getName(),this.tokenID,this.healthStatus));
+            tokenManager.sendMessage(new UpdatedHealth(Entity.EMPLOYEE_TOKEN,this.tokenID,true));
         }
+        //borrowed to try and get random movement going, honestly It's patchwork to get things working.
+        else if (m instanceof MoveToken)
+        {
+        Point2D pointToBeChanged = changeCoordinates(this.GPSLocation.getX(), this.GPSLocation.getY());
+        boolean isLegal = isLegalMove(pointToBeChanged.getX(), pointToBeChanged.getY());
+        if (isLegal) {
+            this.GPSLocation = pointToBeChanged;
+            tokenManager.sendMessage(new UpdatedLocation(Entity.GUEST_TOKEN, this.tokenID, this.GPSLocation));
+        }
+    }
     }
 }
