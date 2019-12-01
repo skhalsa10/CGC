75281e924f75956a6fc621ab88dda2cf011944fa
Anas Gauba <anas.farooq71@gmail.com>
2019-11-30 21:26:02 -0700

    Added ids of token list to the message.

---
+++ Implementation/src/cgc/utils/messages/MoveCarToSouthPickUp.java
@@ -0,0 +1,4 @@
+package cgc.utils.messages;
+
+public class MoveCarToSouthPickUp {
+}
--- Implementation/src/cgc/utils/messages/TourCarArrivedAtDropOff.java
+++ Implementation/src/cgc/utils/messages/TourCarArrivedAtDropOff.java
@@ -2,6 +2,8 @@ package cgc.utils.messages;
 
 import cgc.utils.LocationStatus;
 
+import java.util.LinkedList;
+
 /**
  * This message will be sent from car to vehicleDispatcher whenever the car arrives at dropoff location.
  */
@@ -9,10 +11,12 @@ public class TourCarArrivedAtDropOff implements Message {
 
     private int carId;
     private LocationStatus dropOffLocation;
+    private LinkedList<Integer> tokensId;
 
-    public TourCarArrivedAtDropOff(int carId, LocationStatus dropOffLocation) {
+    public TourCarArrivedAtDropOff(int carId, LocationStatus dropOffLocation, LinkedList<Integer> tokensId) {
         this.carId = carId;
         this.dropOffLocation = dropOffLocation;
+        this.tokensId = tokensId;
     }
 
     public int getCarId() {
@@ -21,5 +25,9 @@ public class TourCarArrivedAtDropOff implements Message {
 
     public LocationStatus getDropOffLocation() {
         return dropOffLocation;
+    }
+
+    public LinkedList<Integer> getTokensId() {
+        return tokensId;
     }
 }
