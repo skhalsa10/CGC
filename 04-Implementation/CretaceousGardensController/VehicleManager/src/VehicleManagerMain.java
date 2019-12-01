a0cc0ad408b3230d3f20163d32b38b43c77c99c3
Ezequial Ramos <xerxiesseixrex@gmail.com>
2019-11-30 17:04:32 -0700

    added pseduo code ideas

--- 04-Implementation/CretaceousGardensController/VehicleManager/src/VehicleManagerMain.java
+++ 04-Implementation/CretaceousGardensController/VehicleManager/src/VehicleManagerMain.java
@@ -2,6 +2,14 @@ public class VehicleManagerMain
 {
     public static void main(String[] args)
     {
-    
+        // arguments give number of initial patrol and guest cars
+        // showCars list of cars, their ids, and the list of tokens riding in them
+        // dispatch
+        // decommission
+        // reroute
+        // showStatistics
+        // enterEmergencyMode
+        // enterNormalMode
+        // enterMaintenanceMode
     }
 }
